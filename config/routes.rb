ef46419c23d2795fedad4992795f1babd5f34fdc
HirayamaDaiki <daikisoccer.0804@gmail.com>
2018-10-22 10:59:48 +0000

    controller generate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'services/index'
+    get 'services/show'
+    get 'services/new'
+    get 'services/create'
+    get 'services/edit'
+    get 'services/update'
+    get 'services/destroy'
+  end
+  get 'users/show'
+  get 'comments/create'
+  get 'comments/index'
+  get 'services/index'
+  get 'services/show'
+  get 'services/new'
+  get 'services/create'
+  get 'services/edit'
+  get 'services/update'
+  get 'services/destroy'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
