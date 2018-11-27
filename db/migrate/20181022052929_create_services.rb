c8d75afb4fa0ac0405218b18c9a8869040ba51de
Taizo <>
2018-11-27 15:47:02 +0900

    good luck

--- db/migrate/20181022052929_create_services.rb
+++ db/migrate/20181022052929_create_services.rb
@@ -2,9 +2,11 @@ class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
       t.string :service_name
+      t.string :service_releasetime
       t.text :service_summary
       t.string :service_image
       t.string :service_campany
+      t.text :service_link
       t.text :related_link
  	    t.integer :category_id, foreign_key: true
       t.text :tag_list
