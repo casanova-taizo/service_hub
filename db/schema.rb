1817ebe176b410195899d38c243d246edca4ddec
HirayamaDaiki <daikisoccer.0804@gmail.com>
2018-12-17 14:55:44 +0000

    デプロイ優先したい

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_135510) do
+ActiveRecord::Schema.define(version: 2018_12_17_145018) do
 
   create_table "bad_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 2018_12_03_135510) do
     t.string "service_name"
     t.text "service_summary"
     t.string "service_campany"
-    t.text "service_linkr"
     t.text "related_link"
     t.integer "category_id"
     t.text "tag_list"
@@ -135,6 +134,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_135510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 2, null: false
+    t.boolean "guest", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
